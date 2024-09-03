ac55fe88c8abb7508fbf4d9db59c2c313da984f7
Aniket Singh <aniketsingh90126@gmail.com>
2024-09-03 08:13:47 +0530

    fix: login and logout redirect issue

--- README.md
+++ README.md
@@ -15,11 +15,15 @@
 -created our redux store with userSlice
 -implement sign out
 -update profile 
+-BugFix - sign up user displayName and profile picture update
+-BugFix - if user is not logged in redirect him to the login page and if logged in redirect him to browse page 
+-unsubscribed to onAuthStateChange callback
+-add constants for images url
 
 # features
 -Login/Sign up
     -Sign In /Sign up form
-    -redirect to Browse Page
+    -redirect to Browse Page    
 
 -Browse (after authentication)
     -Main Movie
--- src/assets/components/Body.jsx
+++ src/assets/components/Body.jsx
@@ -1,19 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Login from "./Login";
 import Browse from "./Browse";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useNavigate,
-} from "react-router-dom";
-import { auth } from "../utils/firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/Store/userSlice";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Body = () => {
-  const dispatch = useDispatch();
-
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -24,19 +14,6 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // user is signed in
-        const { uid, email, displayName, photoURL } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL }));
-      } else {
-        // User is signed out
-        dispatch(removeUser());
-      }
-    });
-  }, []);
 
   return (
     <div>
